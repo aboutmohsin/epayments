0aff8662fd57e14c44f598a61dd164421d2ebbc4
Mohsin Hassan <codewithmohsinhassan@gmail.com>
2023-04-16 06:43:28 +0500

    create property instruction component. Then add and design the property instructions

---
+++ client/src/components/Instruction/Instruction.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import "./Instruction.css";
+const Instruction = () => {
+  return (
+    <div className="property__instruction">
+      <details className="instruction__details">
+        <summary>PROPERTY'S INSTRUCTIONS YOU NEED TO KNOW</summary>
+        <h2>You need to know</h2>
+        <li>Couples wishing to share a room must provide proof of marriage.</li>
+        <li>
+          Up to 2 children 1 year old and younger stay free when occupying the
+          parent or guardian's room, using existing bedding.
+        </li>
+        <li>
+          This property advises that enhanced cleaning and guest safety measures
+          are currently in place.
+        </li>
+        <li>
+          Disinfectant is used to clean the property; commonly-touched surfaces
+          are cleaned with disinfectant between stays; bed sheets and towels are
+          laundered at a temperature of at least 60°C/140°F
+        </li>
+        <li>
+          Social distancing measures are in place; staff at the property wear
+          personal protective equipment; periodic temperature checks are
+          conducted on staff.
+        </li>
+        <li>Contactless check-in and contactless check-out are available.</li>
+        <li>
+          Each guestroom is kept vacant for a minimum of 24 hours between
+          bookings.
+        </li>
+        <li>
+          This property affirms that it adheres to the cleaning and disinfection
+          practices of Safe Travels (WTTC - Global).
+        </li>
+        <h2>Special Instructions</h2>
+        <p>
+          This property offers transfers from the airport (surcharges may
+          apply). Guests must contact the property with arrival details before
+          travel, using the contact information on the booking confirmation.
+          Front desk staff will greet guests on arrival.
+        </p>
+        <h2>Instructions</h2>
+        <li>
+          Extra-person charges may apply and vary depending on property policy
+        </li>
+        <li>
+          Government-issued photo identification and a credit card, debit card,
+          or cash deposit may be required at check-in for incidental charges
+        </li>
+        <li>
+          Special requests are subject to availability upon check-in and may
+          incur additional charges; special requests cannot be guaranteed
+        </li>
+        <li>
+          Safety features at this property include a fire extinguisher, a smoke
+          detector, a security system, a first aid kit, and window guards
+        </li>
+        <li>
+          Be prepared: check the latest COVID-19 travel requirements and
+          measures in place for this destination before you travel.
+        </li>
+        <li>
+          Please note that cultural norms and guest policies may differ by
+          country and by property; the policies listed are provided by the
+          property
+        </li>
+        <h2>Optional fees</h2>
+        <li>Airport shuttle fee: USD 17 per vehicle (one-way)</li>
+        <li>
+          Airport shuttle fee per child: USD 0 (one-way), (from 1 to 12 years
+          old)
+        </li>
+        <li>Rollaway bed fee: USD 5 per day</li>
+        <p>
+          The above list may not be comprehensive. Fees and deposits may not
+          include tax and are subject to change.
+        </p>
+        <h2>Spoken Language</h2>
+        <li>Arabic</li>
+        <li>Urdu</li>
+        <li>English</li>
+      </details>
+    </div>
+  );
+};
+
+export default Instruction;
