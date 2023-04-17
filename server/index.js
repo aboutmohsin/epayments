ffc8345c8b59983500202e849b619bdedd16ec8f
Mohsin Hassan <codewithmohsinhassan@gmail.com>
2023-04-18 00:55:11 +0500

    cors issue

--- server/index.js
+++ server/index.js
@@ -18,23 +18,13 @@ app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/", Router);
-app.use(cors());
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
-// app.use(
-//   cors({
-//     origin: "*",
-//     methods: ["GET,POST,PUT,DELETE"],
-//   })
-// );
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET,POST,PUT,DELETE"],
+  })
+);
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () =>
   console.log(`Server is running successfull on PORT: ${PORT}`)
